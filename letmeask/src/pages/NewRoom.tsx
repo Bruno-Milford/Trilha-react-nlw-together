67e124aa92c60bb557e6c54c8e29a5bbda086d2b
Trynda-LOL <bm33518@gmail.com>
2021-06-24 09:55:17 -0300

    authentication with google

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,6 +1,8 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
+import { Link } from 'react-router-dom';
+
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
@@ -21,7 +23,7 @@ export function NewRoom() {
             <input type="text" placeholder="Nome da sala" />
             <Button type="submit">Criar sala</Button>
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+          <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
         </div>
       </main>
     </div>
